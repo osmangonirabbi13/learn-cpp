e9aa21bde2cd59f17d5fda5d1442c0a9a7296375
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-09-07 15:17:37 +0600

    reverse n

--- while-loop.cpp
+++ while-loop.cpp
@@ -5,15 +5,24 @@ int main()
 // Qs. Print the sum of digits of a number using while loop .
 {
     int n = 10829;
-    int sum = 0;
-    while (n > 0)
+    // int sum = 0;
+    // while (n > 0)
+    // {
+    //     int lastDisit = n % 10;
+    //     sum += lastDisit;
+    //     n = n / 10;
+    // }
+
+    // cout << " Total Sum  : " << sum << endl;
+
+    // reverse n
+
+    while (n >= 1)
     {
-        int lastDisit = n % 10;
-        sum += lastDisit;
+        int lastDis = n % 10;
+        cout << lastDis;
         n = n / 10;
     }
-
-    cout << " Total Sum  : " << sum << endl;
 
     return 0;
 }

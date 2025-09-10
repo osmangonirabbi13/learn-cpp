078cd0cd54350d148365e662611d246f12093260
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-09-10 14:35:53 +0600

    funcation even number check

---
+++ funcationEven.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isEven(int n)
+{
+    if (n % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    cout << isEven(10) << endl;
+    return 0;
+}

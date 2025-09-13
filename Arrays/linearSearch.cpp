e7f0e59ecf32ab68a433c70218136fe83420dabc
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-09-13 17:51:56 +0600

    linearSearch

---
+++ Arrays/linearSearch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int *arr, int n, int key)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18};
+    int n = sizeof(arr) / sizeof(int);
+    cout << linearSearch(arr, n, 10);
+    return 0;
+}

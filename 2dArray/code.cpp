bcc836c3d0311a15d9b59aa75f8b957ad55dd5a6
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-09-20 10:05:10 +0600

    2d array

---
+++ 2dArray/code.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int student[3][3] = {{100, 200, 300},
+                         {70, 60, 90},
+                         {10, 20, 30}};
+    cout << student[1][1] << endl;
+}
